1882b3219e9e1b041ffe84b87c3d22664c76fa85
Jakub Bateľ <jakub.batel.jb@gmail.com>
2021-05-27 13:46:21 +0200

    profile avatar

---
+++ lib/components/profile_circle_avatar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileCircleAvatar extends StatelessWidget {
+
+  final String pictureUrl;
+  final double radius;
+
+  ProfileCircleAvatar({this.pictureUrl, this.radius});
+
+  @override
+  Widget build(BuildContext context) {
+    if (pictureUrl == null || pictureUrl == '') {
+      return CircleAvatar(
+        radius: radius,
+        child: Icon(Icons.perm_identity_outlined, size: radius),
+      );
+    }
+    return CircleAvatar(
+      radius: radius,
+      backgroundImage: NetworkImage(pictureUrl),
+    );
+  }
+}
