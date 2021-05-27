ea90a877e5986f69e83d3b88d5fd457cbea2bbff
Jakub Bateľ <jakub.batel.jb@gmail.com>
2021-05-27 21:43:38 +0200

    equals and hash code added to models

--- lib/model/location_model.dart
+++ lib/model/location_model.dart
@@ -1,7 +1,15 @@
+import 'package:quiver/core.dart';
+
 class LocationModel {
 
   final double latitude;
   final double longitude;
+
+  @override
+  bool operator==(Object other) => other is LocationModel && latitude == other.latitude && longitude == other.longitude;
+
+  @override
+  int get hashCode => hashObjects([latitude, longitude]);
 
   LocationModel({this.latitude, this.longitude});
 
